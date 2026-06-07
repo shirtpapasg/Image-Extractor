6b4b5890fabd5f5cda20c8502a419e9d9a3293ea
shirtpapasg <shirtpapasg@gmail.com>
2026-06-08 06:36:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import * as pdfjsLib from "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.min.mjs";
 
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.mjs";
+
 let file = null;
 
 const drop = document.getElementById("drop");
